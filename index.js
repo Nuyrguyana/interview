ea883a8e333a8182562a33f305ef87bd24c19cbb
MichaelD <enee.kors@mail.ru>
2022-02-13 13:21:53 +0300

    adding some Q&A

---
+++ index.js
@@ -0,0 +1,29 @@
+const wizards = [
+    {
+        name: 'Harry Potter',
+        house: 'Gryfindor'
+    },
+    {
+        name: 'Cedric Diggory',
+        house: 'Hufflepuff'
+    },
+    {
+        name: 'Tonks',
+        house: 'Hufflepuff'
+    },
+    {
+        name: 'Ronald Weasley',
+        house: 'Gryfindor'
+    },
+    {
+        name: 'Hermione Granger',
+        house: 'Gryfindor'
+    }];
+
+const hufflepuff = wizards.filter(function (wizard) {
+    return wizard.house === 'Hufflepuff';
+}).map(function (wizard) {
+    return wizard.name;
+});
+
+console.log(hufflepuff)
